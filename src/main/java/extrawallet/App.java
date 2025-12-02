8f02486739c201c48dab07ccdfa391bc12a581d7
trihajikhr <trihajikhr@gmail.com>
2025-12-02 21:11:20 +0700

    setup javafx pertama

--- src/main/java/extrawallet/App.java
+++
@@ -1,9 +0,0 @@
-package extrawallet;
-
-public class App {
-  public static void main(String[] args) {
-
-      System.out.println("Hello World!");
-      System.out.println("Halo kawan");
-  }
-}
--- src/main/java/extrawallet/Main.java
+++
@@ -1,4 +0,0 @@
-package extrawallet;
-
-public class Main {
-}
