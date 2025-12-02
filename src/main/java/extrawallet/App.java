e8a234bc9c9418dd1fb3585407a35fb556213218
trihajikhr <trihajikhr@gmail.com>
2025-12-02 18:20:22 +0700

    fix pom.file

--- src/main/java/extrawallet/App.java
+++ src/main/java/extrawallet/App.java
@@ -2,6 +2,8 @@ package extrawallet;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
+      System.out.println("Hello World!");
+      System.out.println("Halo kawan");
   }
 }
