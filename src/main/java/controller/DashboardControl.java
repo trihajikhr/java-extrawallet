181c16ff072b255e56c41b96b0b9960c75c4c575
trihajikhr <trihajikhr@gmail.com>
2025-12-09 20:07:02 +0700

    backup

--- src/main/java/controller/DashboardControl.java
+++ src/main/java/controller/DashboardControl.java
@@ -1,68 +1,38 @@
 package controller;
 
 import javafx.scene.layout.AnchorPane;
-import javafx.animation.Interpolator;
-import javafx.scene.control.Button;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.util.Duration;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import javafx.scene.Parent;
 import javafx.fxml.FXML;
 
 public class DashboardControl {
 
-    @FXML
-    private AnchorPane labelSidebar;
+    @FXML private AnchorPane corePane;
+
+    @FXML private void home(ActionEvent e){loadPage("home");}
+    @FXML private void income(ActionEvent e){loadPage("income");}
+    @FXML private void expense(ActionEvent e){loadPage("expense");}
+    @FXML private void statistic(ActionEvent e){loadPage("statistic");}
+
+//    @FXML
+//    private void addTransaction(MouseEvent event) throws IOException {
+//        PopupUtils.showPopup("/fxml/transaction.fxml", (Node) event.getSource());
+//    }
 
     @FXML
-    private AnchorPane mainContent;
-
-    @FXML
-    private Button toggleButton;
-
-    // boolean untuk memberi tahu apakah sidebar terbuka atau tidak
-    private boolean open = false;
-
-    // variabel global dan final
-    private final double SIDEBAR_WIDTH = 200;
-    private final double ANCHOR_ICON_WIDTH = 60;
-    private final Duration D = Duration.millis(220);
-
-    @FXML
-    private final double BUTTON_SHIFT = SIDEBAR_WIDTH;
-
-    // inisialisasi
-    @FXML
-    public void initialize() {
-        // ketika baru buka app, sidebar hidden
-        labelSidebar.setTranslateX(-SIDEBAR_WIDTH);
-        mainContent.setTranslateX(0);
-        toggleButton.setText("≡");
-    }
-
-    // fungsi sidebar open dan tutup, diset OnAction:toggleSidebar
-    @FXML
-    public void toggleSidebar() {
-        toggleButton.setDisable(true);;
-
-        double labelTarget = open ? -SIDEBAR_WIDTH : 0;
-        double contentTarget = open ? 0 : SIDEBAR_WIDTH;
-
-        double buttonTarget = open ? 0 : BUTTON_SHIFT;
-
-        KeyValue kvLabel = new KeyValue(labelSidebar.translateXProperty(), labelTarget, Interpolator.EASE_BOTH);
-        KeyValue kvContent = new KeyValue(mainContent.translateXProperty(), contentTarget, Interpolator.EASE_BOTH);
-        KeyValue kvButton = new KeyValue(toggleButton.translateXProperty(), buttonTarget, Interpolator.EASE_BOTH);
-
-        KeyFrame kf = new KeyFrame (D, kvLabel, kvContent, kvButton);
-        Timeline tl = new Timeline(kf);
-
-        tl.setOnFinished(evt -> {
-            open = !open;
-            toggleButton.setText(open ? "←" : "≡");
-            toggleButton.setDisable(false);
-        });
-
-        tl.play();
+    public void loadPage(String page) {
+        try {
+            FXMLLoader loadder = new FXMLLoader(getClass().getResource("/fxml/" + page + ".fxml"));
+            Parent root = loadder.load();
+            corePane.getChildren().setAll(root);
+            AnchorPane.setTopAnchor(root, 0.0);
+            AnchorPane.setBottomAnchor(root, 0.0);
+            AnchorPane.setLeftAnchor(root, 0.0);
+            AnchorPane.setRightAnchor(root, 0.0);
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+        }
     }
 }
