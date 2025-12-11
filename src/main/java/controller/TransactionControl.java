6ba2ad828718870453ac12e2da170d91519177a7
trihajikhr <trihajikhr@gmail.com>
2025-12-11 23:39:29 +0700

    efek fadeIn fadeOut + zoomIn zoomOut pop up transaksi

--- src/main/java/controller/TransactionControl.java
+++ src/main/java/controller/TransactionControl.java
@@ -1,12 +1,19 @@
 package controller;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -15,6 +22,10 @@ public class TransactionControl implements Initializable {
 
     private Stage stage;
     private int valueChoosen = 0;
+    private boolean closing = false;
+
+    @FXML
+    private AnchorPane rootPane;
 
     @FXML private Button closeButton;
     @FXML private Button incomeBtn;
@@ -38,7 +49,25 @@ public class TransactionControl implements Initializable {
 
     @FXML
     private void closePopup() {
-        if (stage != null) stage.close();
+        if (closing) return;
+        closing = true;
+        if (stage == null) return;
+
+        FadeTransition fade = new FadeTransition(Duration.millis(150), rootPane);
+        fade.setFromValue(1);
+        fade.setToValue(0);
+
+        ScaleTransition scale = new ScaleTransition(Duration.millis(150), rootPane);
+        scale.setFromX(1);
+        scale.setFromY(1);
+        scale.setToX(0.8);
+        scale.setToY(0.8);
+
+        ParallelTransition hideAnim = new ParallelTransition(fade, scale);
+        hideAnim.setInterpolator(Interpolator.EASE_BOTH);
+
+        hideAnim.setOnFinished(e -> stage.close());
+        hideAnim.play();
     }
 
     @Override
@@ -62,6 +91,26 @@ public class TransactionControl implements Initializable {
 
         initButtons();
         clearSelection(); // default: semua putih, teks hitam, icon hitam
+
+        rootPane.setOpacity(0);
+
+        rootPane.setScaleX(0.8);
+        rootPane.setScaleY(0.8);
+        rootPane.setOpacity(0);
+
+        FadeTransition fade = new FadeTransition(Duration.millis(200), rootPane);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+
+        ScaleTransition scale = new ScaleTransition(Duration.millis(200), rootPane);
+        scale.setFromX(0.8);
+        scale.setFromY(0.8);
+        scale.setToX(1);
+        scale.setToY(1);
+
+        ParallelTransition showAnim = new ParallelTransition(fade, scale);
+        showAnim.setInterpolator(Interpolator.EASE_BOTH);
+        showAnim.play();
     }
 
     private void initButtons() {
